f73f7a7c8cb32bf4b37494803bc76420a10074cc
Vishal Buwade <82523633+vishalbuwade17@users.noreply.github.com>
2025-03-16 13:43:35 +0000

    datatypes

---
+++ 0-1_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict" // treat all javascript code as newer version
+
+// alert(3+3) , // we are using node js not browser
+/*primitive data types=Primitive data types are the built-in data types provided by JavaScript.
+ They represent single values and are not mutable=Their values can be modified after creation */
+
+let name ="vishal" //string
+let age = 22 //number
+let isLoggedin =false //boolean
+
+//number => 2 to power 53
+//bigInt
+//string
+//boolean
+//null
+//undefined
+//symbol= It is used when we want to uniquely Identify something
+
+console.log(typeof age)
+console.log(typeof name)
+console.log(typeof null)
+console.log(typeof undefined)
