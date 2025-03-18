b91fc8731c68848066807971d77a61891ba35524
Vishal Buwade <82523633+vishalbuwade17@users.noreply.github.com>
2025-03-18 09:41:07 +0000

    conversionOperation

---
+++ 0-1_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+//let score="123ab";if we change to number if we see console.log and
+//  see type of it will give (number) but if we directly console.log it will give (nan).
+
+//let score = null(object) if we change to number if we see console.log and
+//  see type of it will give (number) but if we directly console.log it will give (0).
+
+//let score = undefined() if we change to number if we see console.log and
+//  see type of it will give (number) but if we directly console.log it will give (nan).
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let numberChange = Number(undefined)
+// console.log(typeof numberChange)
+// console.log(numberChange)
+
+// 33 >= 33
+// "123abc" >= NaN
+// true >= 1 or false >= 0
+
+
+// 1 => true
+// "hitesh" => true
+// " " => false
+
+// let someString = 33
+// let convertString = String(someString)
+// console.log(convertString)
+// console.log(typeof convertString)
+
+//********************* Operations **************************
+
+console.log("ab")
