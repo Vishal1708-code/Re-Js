086ab33162e552dac6c1aa41f01d5404c0d584c7
Vishal Buwade <82523633+vishalbuwade17@users.noreply.github.com>
2025-03-20 10:13:05 +0000

    numsAndmaths

---
+++ 0-1_basics/06_numsAndmaths.js
@@ -0,0 +1,21 @@
+const score = 500;
+console.log(score);
+
+const secondNumber =new  Number(400)
+console.log(secondNumber.toString());
+console.log(typeof secondNumber);
+
+const thirdNumber = 400.19543;
+console.log(thirdNumber.toPrecision(3));
+
+const fourthNumber = -4;
+console.log(Math.abs(fourthNumber));
+console.log(Math.floor(4.9));
+console.log(Math.ceil(4.1));
+console.log(Math.round(4.5));
+console.log(Math.round(4.4));
+console.log(Math.pow(2, 3));
+console.log(Math.sqrt(16));
+console.log(Math.min(1, 2, 3, 4, 5));
+console.log(Math.max(1, 2, 3, 4, 5));
+console.log((Math.random()*10)+1);
