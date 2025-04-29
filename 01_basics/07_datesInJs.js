11704d06d9f81b732b93ed4d9e8b458d6df714cd
Vishal Buwade <82523633+vishalbuwade17@users.noreply.github.com>
2025-04-29 10:16:35 +0000

    changes

---
+++ 01_basics/07_datesInJs.js
@@ -0,0 +1,15 @@
+let myDate = new Date();
+console.log(myDate.toJSON());
+
+console.log(myDate.toString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleDateString());
+
+let myTimestamp = Date.now( );
+console.log(myTimestamp);
+ 
+console.log(myDate.getTime());
+//in second
+console.log(myDate.getTime()/1000);
+
+//More customized date
