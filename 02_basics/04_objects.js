480c8c7fd09b0e1b424e3651c14b6758ab51d0a9
Vishal Buwade <82523633+vishalbuwade17@users.noreply.github.com>
2025-04-30 12:43:32 +0000

    changes

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -38,3 +38,22 @@ const regularUser = {
 };
 
 console.log(regularUser.Userfullname);
+// console.log(regularUser.Userfullname);
+
+// Singleton object example (not required here)
+// const tinder = new Object(); // Optional singleton
+// tinder.name = "Arjun";
+// tinder.age = 20;
+// tinder.isloggedIn = false;
+// console.log(Object.keys(tinder));
+// console.log(Object.values(tinder)); // ["name", "age", "isloggedIn"]
+
+
+ 
+
+//combining objects
+
+// let obj1 = {1:"a", 2:"b"};
+// let obj2 = {3:"c", 4:"d"};
+// let obj3 ={...obj1, ...obj2};
+// console.log(obj3); // {1: "a", 2: "b", 3: "c", 4: "d"}
