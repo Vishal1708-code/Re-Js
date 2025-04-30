4a6c83725364e44a534c43d13da6116aa482bd28
Vishal Buwade <82523633+vishalbuwade17@users.noreply.github.com>
2025-04-30 20:09:36 +0000

    changesfile

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -20,22 +20,22 @@
 // console.log(regularUser.Userfullname);
 
 // Singleton object example (not required here)
-const tinder = new Object(); // Optional singleton
-tinder.name = "Arjun";
-tinder.age = 20;
-tinder.isloggedIn = false;
-console.log(tinder);
+// const tinder = new Object(); // Optional singleton
+// tinder.name = "Arjun";
+// tinder.age = 20;
+// tinder.isloggedIn = false;
+// console.log(tinder);
 
-const regularUser = {
-    name: "karan",
-    Userfullname: {
-        firstName: "Karan",
-        lastName: "Joshi",
-        style: {
-            current: "NO"
-        }
-    }
-};
+// const regularUser = {
+//     name: "karan",
+//     Userfullname: {
+//         firstName: "Karan",
+//         lastName: "Joshi",
+//         style: {
+//             current: "NO"
+//         }
+//     }
+// };
 
 console.log(regularUser.Userfullname);
 // console.log(regularUser.Userfullname);
@@ -57,3 +57,12 @@ console.log(regularUser.Userfullname);
 // let obj2 = {3:"c", 4:"d"};
 // let obj3 ={...obj1, ...obj2};
 // console.log(obj3); // {1: "a", 2: "b", 3: "c", 4: "d"}
+
+const course = {
+    CouseName: "JavaScript",
+    price: 199,
+    isPaid: true,
+}
+
+const{price} = course;// destructuring
+console.log(price); // 199
