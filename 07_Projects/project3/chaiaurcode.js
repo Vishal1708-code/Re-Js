9bca5300deba66c1fbd0c5e4b24414379bbc16fc
Vishal Buwade <82523633+vishalbuwade17@users.noreply.github.com>
2025-05-19 20:24:22 +0000

    yes

--- 07_Projects/project3/chaiaurcode.js
+++ 07_Projects/project3/chaiaurcode.js
@@ -2,7 +2,7 @@ let clock = document.getElementById('clock');
 
 setInterval(function(){
     let date = new Date();
-clock.innerHTML = date.toLocaleString();
-
-
-},1000);
+    if (clock) {
+        clock.innerHTML = date.toLocaleString();
+    }
+}, 1000);
